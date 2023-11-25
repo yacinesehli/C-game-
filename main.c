ff2ce528b3dbcd66d287d5d83cb12352ed99eeeb
yacinesehli <152006722+yacinesehli@users.noreply.github.com>
2023-11-25 13:21:28 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,152 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <windows.h>
+
+#define WIDTH 20
+#define HEIGHT 10
+
+int gameOver;
+int score;
+int tailX[100], tailY[100];
+int nTail;
+int x, y, fruitX, fruitY, direction;
+
+void setup() {
+    gameOver = 0;
+    score = 0;
+    x = WIDTH / 2;
+    y = HEIGHT / 2;
+    fruitX = rand() % WIDTH;
+    fruitY = rand() % HEIGHT;
+    direction = 0; // initially not moving
+}
+
+void draw() {
+    system("cls"); // clear the console
+
+    for (int i = 0; i < WIDTH + 2; i++)
+        printf("#");
+    printf("\n");
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (j == 0)
+                printf("#");
+
+            if (i == y && j == x)
+                printf("O");
+            else if (i == fruitY && j == fruitX)
+                printf("F");
+            else {
+                int printTail = 0;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        printf("o");
+                        printTail = 1;
+                    }
+                }
+                if (!printTail)
+                    printf(" ");
+            }
+
+            if (j == WIDTH - 1)
+                printf("#");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < WIDTH + 2; i++)
+        printf("#");
+    printf("\n");
+
+    printf("Score: %d", score);
+}
+
+void input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+            case 'a':
+                direction = 1;
+                break;
+            case 'd':
+                direction = 2;
+                break;
+            case 'w':
+                direction = 3;
+                break;
+            case 's':
+                direction = 4;
+                break;
+            case 'x':
+                gameOver = 1;
+                break;
+        }
+    }
+}
+
+void algorithm() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (direction) {
+        case 1:
+            x--;
+            break;
+        case 2:
+            x++;
+            break;
+        case 3:
+            y--;
+            break;
+        case 4:
+            y++;
+            break;
+    }
+
+    if (x < 0)
+        x = WIDTH - 1;
+    else if (x >= WIDTH)
+        x = 0;
+
+    if (y < 0)
+        y = HEIGHT - 1;
+    else if (y >= HEIGHT)
+        y = 0;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = 1;
+
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % WIDTH;
+        fruitY = rand() % HEIGHT;
+        nTail++;
+    }
+}
 
 int main() {
-    int number_to_guess, player_guess, attempts = 0;
-    srand(time(NULL));
-    number_to_guess = rand() % 100 + 1;
+    setup();
 
-    printf("Welcome to the Number Guessing Game!\n");
-    printf("Try to guess the number between 1 and 100.\n");
-
-    do {
-        printf("Enter your guess: ");
-        scanf("%d", &player_guess);
-        attempts++;
-        if (player_guess == number_to_guess) {
-            printf("Congratulations! You guessed the number %d in %d attempts.\n", number_to_guess, attempts);
-        } else if (player_guess < number_to_guess) {
-            printf("Too low! Try again.\n");
-        } else {
-            printf("Too high! Try again.\n");
-        }
-
-    } while (player_guess != number_to_guess);
+    while (!gameOver) {
+        draw();
+        input();
+        algorithm();
+        Sleep(100); // Adjust the speed of the game
+    }
 
     return 0;
 }
